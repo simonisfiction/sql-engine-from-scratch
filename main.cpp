8db77a192f5ccad24ee4014a02de60e9df9d98e9
simonisfiction <simon.chesney@yahoo.com>
2025-10-27 03:31:40 -0700

    first commit. create main file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	
+	bool runProgram = true;
+	sqlInstruction = "";
+	while(runProgram){
+		cin.getline(sqlInstruction);
+		cout << sqlInstruction;	
+	}
+
+	return 0;
+}
